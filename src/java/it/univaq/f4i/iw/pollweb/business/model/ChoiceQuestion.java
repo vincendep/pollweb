93195341341b65260489c81120ab0183f294f3a6
vincendep <vincenzo.depetris@student.univaq.it>
2020-01-22 17:07:20 +0100

    .

--- src/java/it/univaq/f4i/iw/pollweb/business/model/ChoiceQuestion.java
+++ src/java/it/univaq/f4i/iw/pollweb/business/model/ChoiceQuestion.java
@@ -19,7 +19,11 @@ public class ChoiceQuestion extends Question {
     private List<Option> options = new ArrayList<>();
 
     public ChoiceQuestion() {
-        this.minNumberOfChoices = 0;
+        if (this.isMandatory()) {
+            this.minNumberOfChoices = 1;
+        } else {
+            this.minNumberOfChoices = 0;
+        }
         this.maxNumberOfChoices = 50;
     }
 
